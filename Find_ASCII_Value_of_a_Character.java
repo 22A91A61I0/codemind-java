e7a24d8d492775a869268e2c6fc4a247c6054646
divyasri <119840453+22A91A61I0@users.noreply.github.com>
2023-08-05 11:44:03 +0530

    import java.util.Scanner; public class multi {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         char n1=sc.next().charAt(0);         int asc=n1;         System.out.printf("%d",asc);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class multi
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char n1=sc.next().charAt(0);
+        int asc=n1;
+        System.out.printf("%d",asc);
+    }
+}
