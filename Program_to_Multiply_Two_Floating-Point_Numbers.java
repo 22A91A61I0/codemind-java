6cd52093bf558f9cd1055524ea832cbb044761ed
divyasri <119840453+22A91A61I0@users.noreply.github.com>
2023-08-05 11:04:13 +0530

    import java.util.Scanner; public class multi {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         float n1=sc.nextFloat();         float n2=sc.nextFloat();         float mul=(n1*n2);         System.out.printf("%.2f",mul);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class multi
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float n1=sc.nextFloat();
+        float n2=sc.nextFloat();
+        float mul=(n1*n2);
+        System.out.printf("%.2f",mul);
+    }
+}
