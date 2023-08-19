c953abdb9d03f93db04c3db0ec48c7c66bd95a8f
divyasri <119840453+22A91A61I0@users.noreply.github.com>
2023-08-19 11:22:50 +0530

    import java.util.Scanner; public class can {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         if(x!=0)         {             if(x%2==0)             {                 System.out.println("YES");             }             else             {                 System.out.println("NO");             }         }         else         {             if(y%2==0)             {                 System.out.println("YES");             }             else             {                 System.out.println("NO");             }         }     } }

---
+++ Can_share_equally?.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class can
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if(x!=0)
+        {
+            if(x%2==0)
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+        }
+        else
+        {
+            if(y%2==0)
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+        }
+    }
+}
