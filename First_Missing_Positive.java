2e958d663d193634e6a7d864a505c0ca9cdf18f0
divyasri <119840453+22A91A61I0@users.noreply.github.com>
2023-09-09 20:53:03 +0530

    import java.util.Scanner; public class FirstMissingPositive {     public static int firstMissingPositive(int[] nums) {         int n = nums.length;         for (int i = 0; i < n; i++) {             while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {                 int temp = nums[i];                 nums[i] = nums[temp - 1];                 nums[temp - 1] = temp;             }         }         for (int i = 0; i < n; i++) {             if (nums[i] != i + 1) {                 return i + 1;             }         }         return n + 1;     }     public static void main(String[] args) {         Scanner scanner = new Scanner(System.in);         int n = scanner.nextInt();         int[] nums = new int[n];         for (int i = 0; i < n; i++) {             nums[i] = scanner.nextInt();         }         int missingPositive = firstMissingPositive(nums);         System.out.println(missingPositive);         scanner.close();     } }

---
+++ First_Missing_Positive.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class FirstMissingPositive {
+    public static int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        int missingPositive = firstMissingPositive(nums);
+        System.out.println(missingPositive);
+        scanner.close();
+    }
+}
