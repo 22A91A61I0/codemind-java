b8b66fba91baa7f979dce8c894bf2283b87b18d2
divyasri <119840453+22A91A61I0@users.noreply.github.com>
2023-09-10 18:45:41 +0530

    import java.util.Scanner; public class mat {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int r=sc.nextInt();         int c=sc.nextInt();         int[][] arr=new int[r][c];         int i,j;         for(i=0;i<r;i++)         {             for(j=0;j<c;j++)             {                 arr[i][j]=sc.nextInt();             }         }         int max=0;         for(i=0;i<c;i++)         {             int sum=0;             for(j=0;j<r;j++)             {                 sum+=arr[j][i];             }             if(sum>max)             {                 max=sum;             }         }         System.out.print(max);     } }

---
+++ Maximum_sum_of_column_wise_sum_of_a_matrix.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class mat
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        int[][] arr=new int[r][c];
+        int i,j;
+        for(i=0;i<r;i++)
+        {
+            for(j=0;j<c;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int max=0;
+        for(i=0;i<c;i++)
+        {
+            int sum=0;
+            for(j=0;j<r;j++)
+            {
+                sum+=arr[j][i];
+            }
+            if(sum>max)
+            {
+                max=sum;
+            }
+        }
+        System.out.print(max);
+    }
+}
