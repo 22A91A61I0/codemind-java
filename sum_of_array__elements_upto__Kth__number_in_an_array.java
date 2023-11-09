e54f285576e1729f27565c103d6e79e60479f1af
divyasri <119840453+22A91A61I0@users.noreply.github.com>
2023-11-09 19:51:44 +0530

    import java.util.Scanner; public class bet {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int a=sc.nextInt();         int sum=0;         for(int i=0;i<n;i++)         {             if(i<a)             {                 sum+=arr[i];             }         }         System.out.print(sum);     } }

---
+++ sum_of_array__elements_upto__Kth__number_in_an_array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class bet
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int a=sc.nextInt();
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i<a)
+            {
+                sum+=arr[i];
+            }
+        }
+        System.out.print(sum);
+    }
+}
