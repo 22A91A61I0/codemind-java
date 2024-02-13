7e76c0f1b3a5ef7a1dba09db275e779608ae5fb3
divyasri <119840453+22A91A61I0@users.noreply.github.com>
2024-02-13 21:12:53 +0530

    import java.util.Scanner; public class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         int c=0;         for(int i=0;i<s.length();i++)         {             char ch=s.charAt(i);             if(ch>='A' && ch<='Z')             {                 c+=1;             }         }         System.out.println(c);     } }

---
+++ Uppercase_Letters_in_a_String.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int c=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch>='A' && ch<='Z')
+            {
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
